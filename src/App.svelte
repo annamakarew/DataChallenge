01d46ec1875e19b843d6944113b0569b18c82548
annamakarew <makarewiczanna@yahoo.com>
2025-07-21 20:00:54 -0700

    Sample conclusion added.

--- src/App.svelte
+++ src/App.svelte
@@ -4,11 +4,7 @@
   import Section3 from "./sections/Section3.svelte";
   import Section4 from "./sections/Section4.svelte";
   import TitleSection from "./sections/TitleSection.svelte";
-  import InteresectionObserverBasicExample from "./sections/InteresectionObserverBasicExample.svelte";
-  import IntersectionObserverAlertExample from "./sections/IntersectionObserverAlertExample.svelte";
-  import DisappearingDuckExample from "./sections/DisappearingDuckExample.svelte";
-  import IntersectionObserverSectionCard from "./sections/IntersectionObserverSectionCard.svelte";
-  import IntersectionObserverListExample from "./sections/IntersectionObserverListExample.svelte";
+  import ConclusionSection from "./sections/ConclusionSection.svelte";
 </script>
 
 <main>
@@ -18,9 +14,5 @@
   <Section3 />
   <Section4 />
 
-  <IntersectionObserverSectionCard />
-  <InteresectionObserverBasicExample />
-  <IntersectionObserverAlertExample />
-  <DisappearingDuckExample />
-  <IntersectionObserverListExample />
+  <ConclusionSection />
 </main>
---
+++ src/sections/ConclusionSection.svelte
@@ -0,0 +1,10 @@
+<script>
+  import TitleCard from "../lib/TitleCard.svelte";
+
+  const title =
+    "El Dorado County’s homeownership story reveals that equity isn't just about access — it's about belonging.";
+  const subtitle =
+    "To build a more inclusive future, we must first acknowledge the barriers that have shaped its past.";
+</script>
+
+<TitleCard {title} {subtitle} />
